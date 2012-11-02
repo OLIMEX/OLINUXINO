8577cbd4dff153c3c769594cf216b395f147d267
Tsvetan Usunov <usunov@olimex.com>
2012-11-02 20:18:28 +0200

    add MOD-BT example

---
+++ SOFTWARE/iMX233/MOD-BT/MOD-BT.c
@@ -0,0 +1,129 @@
+/*
+ * untitled.c
+ * 
+ * Copyright 2012 OLIMEX Ltd
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <termios.h>
+#include "uart.h"
+
+int main(int argc, char **argv)
+{
+
+	if(argc == 2)
+	{
+		if(!strcmp(argv[1], "--transfer") || !strcmp(argv[1], "-t"))
+		{
+			int i;
+			int fd;
+			char ch;
+			char temp[100];
+			i = 0; 
+			while(1)
+			{				
+				ch = getche();
+				temp[i++] = ch;
+				if('\n' == ch)
+					break;
+			}
+			temp[i++] = '\r';
+			temp[i] = 0;
+			Serial_Open(&fd, "/dev/ttySP1", B115200);
+			Serial_Send(&fd, temp);
+			Serial_Close(&fd);	
+		}
+		else if(!strcmp(argv[1], "--recieve") || !strcmp(argv[1], "-r"))
+		{
+			int fd;
+			char ch;
+			char buffer[2];
+			
+			buffer[1] = 0;
+			
+			Serial_Open(&fd, "/dev/ttySP1", B115200);
+			while(1)
+			{
+				Serial_Read(&fd, buffer, 1);
+				ch = buffer[0];
+				if(ch == '\r')
+					break;
+				fprintf(stdout, "%c", ch);
+				fflush(stdout);
+			}	
+			
+			Serial_Close(&fd);
+			printf("\n\r");	
+		}
+		else if(!strcmp(argv[1], "--config") || !strcmp(argv[1], "-c"))
+		{
+			int fd;			
+			Serial_Open(&fd, "/dev/ttySP1", B115200);
+			Serial_Send(&fd, "AT+BTAUT=1,0\r");
+			Serial_Close(&fd);			
+			fputs("Configurating...Done\n\r", stdout);
+		}
+		else if(!strcmp(argv[1], "--start") || !strcmp(argv[1], "-s"))
+		{
+			int fd;			
+			Serial_Open(&fd, "/dev/ttySP1", B115200);
+			Serial_Send(&fd, "AT+BTSRV=1\r");
+			Serial_Close(&fd);			
+			fputs("Starting...Done\n\r", stdout);
+		}
+		else if(!strcmp(argv[1], "--stop") || !strcmp(argv[1], "-p"))
+		{
+			int fd;			
+			Serial_Open(&fd, "/dev/ttySP1", B115200);
+			Serial_Send(&fd, "+++");
+			Serial_Close(&fd);			
+			fputs("Stoping...Done\n\r", stdout);
+		}
+		else if(!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))
+		{
+			fputs("\n\
+USAGE: ./mod-bt [PARAMS]\n", stdout);
+		
+			fputs("\n\
+-c, --config\t\tConfigure\n\
+-s, --start\t\tStart the module\n\
+-p, --stop\t\tStop the module\n\
+-t, --transmit\t\tSend a message to BT device.\n\
+-r, --recieve\t\tRecive message\n", stdout);
+		}
+		else
+		{
+			fputs("Unknow parameter!\n", stdout);
+		}
+	}
+	else
+	{
+		fputs("Wrong number of parameters!\n",stdout);
+	}
+	
+	return 0;
+}
+
---
+++ SOFTWARE/iMX233/MOD-BT/uart.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <termios.h>
+#include "uart.h"
+
+
+int Serial_Open(int* fd, char *serial_Name, speed_t baudrate)
+{
+	
+	struct termios serCfg;
+	memset(&serCfg, 0, sizeof(serCfg));
+	if((*fd = open(serial_Name, O_RDWR)) < 0)
+		return -1;
+	else		
+		if(tcgetattr(*fd, &serCfg) != 0)
+			return -1;
+
+	cfsetispeed(&serCfg, baudrate);
+	cfsetospeed(&serCfg, baudrate);
+	cfmakeraw(&serCfg);
+
+	tcflush(*fd, TCIFLUSH);
+	if(tcsetattr(*fd, TCSANOW, &serCfg) != 0)
+		return -1;
+	return 0;
+}
+int Serial_Send(int *fd, char *string)
+{
+	int len;
+	char *buffer;
+	int bytes_sent;
+
+	
+	len = strlen(string);
+	if(len > 255)
+		return -1;
+	buffer = (char*)malloc((len+1)*sizeof(char));
+	if(buffer == NULL)
+		return -1;
+	strcpy(buffer, string);
+	buffer[len] = 0;
+	bytes_sent = write(*fd, buffer,len+1);
+	if(bytes_sent < 0)
+	{
+		close (*fd);
+		return -1;		
+	}
+	else
+	{
+		free(buffer);
+		return 0;
+	}	
+}
+int Serial_Read(int *fd, char *buff, int len)
+{
+	int chars;
+	chars = read(*fd, buff, len);
+	if(chars < 0)
+		return -1;
+	else
+	{
+		buff[1] = 0;		
+		return 0;
+	}	
+}
+int Serial_Close(int *fd)
+{
+		if(close (*fd) < 0)
+			return -1;
+		else
+			return 0;		
+}
+int kbhit(void)
+{
+    struct timeval tv;
+    fd_set fds;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
+}
+#if 1
+int getch(void)
+{
+	struct termios oldattr, newattr;
+	int ch;
+	tcgetattr( STDIN_FILENO, &oldattr );
+	newattr = oldattr;
+	newattr.c_lflag &= ~( ICANON | ECHO );
+	tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+	ch = getchar();
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+	return ch;
+}
+
+int getche(void)
+{
+	struct termios oldattr, newattr;
+	int ch;
+	tcgetattr( STDIN_FILENO, &oldattr );
+	newattr = oldattr;
+	newattr.c_lflag &= ~( ICANON );
+	tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+	ch = getchar();
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+	return ch;
+	}
+#endif
